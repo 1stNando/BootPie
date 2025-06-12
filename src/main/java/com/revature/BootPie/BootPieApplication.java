13384a913909fa2b19bc0bd93c457c128e275897
Fernando Aguilar <sparkmanperspective@gmail.com>
2025-06-12 14:45:10 -0400

    Added Http GET method and modified port number in application.properties file.

--- src/main/java/com/revature/BootPie/BootPieApplication.java
+++ src/main/java/com/revature/BootPie/BootPieApplication.java
@@ -8,14 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.BootPie.models.Pie;
 import com.revature.BootPie.services.PieService;
-
-
-
-
 @SpringBootApplication
+@RestController
 public class BootPieApplication {
 
 	@Value("${spring.application.name}")
@@ -23,6 +22,16 @@ public class BootPieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootPieApplication.class, args);}
+
+		// Http GET
+		@GetMapping
+		public String piePickerHttp(PieService pieService) {
+				Pie randomPie = pieService.getRandomPie();
+
+				return String.format("The pie selected for you is %s with calories equal to %d with %d slices left!", randomPie.getPieName(),
+				randomPie.getCalories(),
+				randomPie.getSlicesAvailable()); 
+		}
 
 		@Bean
 		public CommandLineRunner inspectorBean(ApplicationContext applicationContext) {
